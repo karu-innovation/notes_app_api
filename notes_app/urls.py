db84282a0c8d693072c237e23d8180fb9c2c8922
Kiash254 <codewithkiash@gmail.com>
2022-11-12 13:47:30 +0300

    added api forms

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import NoteView,CreateView,UpdateView,deleteView
+from .views import NoteView,CreateView,UpdateView,deleteView,DetailView
 
 app_name='notes_app'
 
 urlpatterns=[
     path('',NoteView,name='notes'),
     path('create/',CreateView,name='create'),
+    path('detail/<int:pk>/',DetailView,name='detail'),
     path('update/<int:pk>/',UpdateView,name='update'),
     path('delete/<int:pk>/',deleteView,name='delete'),
 ]
--- notes_app/views.py
+++ notes_app/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from .serialaizers import NotesSerializer
 from rest_framework.decorators import api_view
-from django.http import JsonResponse  , HttpResponse
 from rest_framework.response import Response
 from .models import Notes
-
 # Create your views here.
 @api_view(['GET'])
 def NoteView(request):
     notes=Notes.objects.all()
+    serializer=NotesSerializer(notes,many=True)
     return Response(serializer.data)
+#CreateView
 @api_view(['POST'])
 def CreateView(request):
-    notes = Notes.objects.all()
-    serializer = NotesSerializer(notes, many=True)
+    serializer=NotesSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()       
     return Response(serializer.data)
+# DetailView
+@api_view(['GET'])
+def DetailView(request,pk):
+    notes = Notes.objects.get(id=pk)
+    serializer = NotesSerializer(notes, many=False)
+    return Response(serializer.data)
+#UpdateView
 @api_view(['POST'])
 def UpdateView(request,pk):
     notes = Notes.objects.get(id=pk)
@@ -22,7 +30,7 @@ def UpdateView(request,pk):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
-
+#DeleteView
 @api_view(['DELETE'])
 def deleteView(request, pk):
     note = Notes.objects.get(id=pk)
